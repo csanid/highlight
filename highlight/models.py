764e23d87aa390f0d2d2c7d6ec4365359c5bd307
csanid <csanid@github.com>
2023-04-25 11:55:28 -0300

    Add note model

--- highlight/models.py
+++ highlight/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.CharField(max_length=128, blank=True)
+    book_title = models.CharField(max_length=128, blank=True)
+    publisher = models.CharField(max_length=64, blank=True)
+    year = models.IntegerField(blank=True)
+    content = models.CharField(max_length=8500)
+    timestamp = models.DateField(auto_now_add=True)
+    
--- highlight/views.py
+++ highlight/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import NoteForm
 
+def index(request):
+    return render(request, "highlight/index.html")
+
+def login(request):
+    # if request.method == "POST":
+        # some code 
+        # return decorator? ver cómo
+    return render(request, "highlight/login.html")
+
+def logout(request):
+    return render(request, "highlight/login.html")
+
+def register(request):
+    # if request.method == "POST":
+        # register user
+        # show alert
+        # log in user                
+    return render(request, "highlight/register.html")
+
+def add(request):
+    # if request.method == "POST":
+        # validate and add note
+    return render(request, "highlight/add.html")
+
+def edit(request):
+    return render(request, "highlight/edit.html")
+
+def remove(request):
+    return render(request, "highlight/remove.html")
+
+def search(request):
+    return render(request, "highlight/search.html")
